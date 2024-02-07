e09a685322bd1b4a0c8e8cb3f0a19177d4c37f8b
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-02-06 20:54:46 -0500

    fs

--- hello.py
+++ hello.py
@@ -51,7 +51,7 @@ def convert_df_to_csv(df):
 
 
 # Streamlit app
-st.image("https://images.squarespace-cdn.com/content/v1/611645b4405ecc34f75902d4/1630940958363-48IAK69A1MB56BOGE5I7/P07+Secondary+Logo.png?format=2500w")
+st.image("https://images.squarespace-cdn.com/content/v1/611645b4405ecc34f75902d4/1630940958363-48IAK69A1MB56BOGE5I7/P07+Secondary+Logo.png?format=2500w", width = 200)
 st.title("Po7 1:1 Round Robin Meeting Scheduler")
 st.markdown("Creates a custom 1:1 meeting schedule in between group meetings. App uses a round robin format, enabling each person to meet with each other in the most efficient manner")
 
